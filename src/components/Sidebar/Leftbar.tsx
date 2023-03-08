b07a6c6f16d16a2dc12630194a0143bcfcd1ec02
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-03-08 19:53:14 +0600

    bal

--- src/components/Sidebar/Leftbar.tsx
+++ src/components/Sidebar/Leftbar.tsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { BsArrowLeftShort } from "react-icons/bs";
+import { BsArrowLeftShort, BsSearch } from "react-icons/bs";
+
 import { useState } from "react";
 const Leftbar = () => {
   const [open, setOpen] = useState(true);
   return (
     <>
-      <div className={`mt-20 relative ${open ? "w-72" : "w-20"} duration-700`}>
-        <h1>leftbar</h1>
+      <div
+        className={`mt-20 relative ${open ? "w-72" : "w-20"} duration-700 p-2`}
+      >
+        <h1 className="text-center">Satalite</h1>
         <BsArrowLeftShort
           onClick={() => setOpen(!open)}
           className={`bg-white text-dirkPurple text-3xl rounded-full absolute -right-1 top-9 border-2  cursor-pointer ${
             !open && "rotate-180"
           }`}
         />
+        <div
+          className={`flex items-center rounded-md ${
+            open && "bg-lightWhite"
+          }  mt-10 px-4 py-2`}
+        >
+          <BsSearch
+            className={`text-white text-2xl block float-left cursor-pointer mr-2`}
+          />
+          <input
+            type="search"
+            placeholder="search"
+            className={`text-base bg-transparent w-full text-white focus:outline-none ${
+              !open && "hidden"
+            }`}
+          />
+        </div>
       </div>
     </>
   );
