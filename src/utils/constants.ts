b707c69bff6e7d44a73a921b5723f044b85858fe
naseem(T_T) <ashfaqnaseem1@gmail.com>
2023-08-07 15:51:25 +0600

    well time to find out

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,5 @@ export const camera_init_pos = [0, 0, radius * 3] as const;
 export const one_second = 1000;
 export const intervalTimer = one_second;
 export const SERVER_URL = import.meta.env.DEV
-  ? import.meta.env.BACKEND_URL_DEV
-  : import.meta.env.BACKEND_URL;
+  ? "http://localhost:8000"
+  : "https://satellite-tracker-backend.onrender.com/";
